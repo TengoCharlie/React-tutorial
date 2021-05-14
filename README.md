71b61820ca87aa959a6dcbe3dbd1f094353552b3
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-05-15 03:19:39 +0530

    List and Keys

--- README.md
+++ README.md
@@ -1,8 +1,71 @@
-## Preventing Component from Rendering
+# List and Keys
 
-In rare cases you might want a component to hide itself even though it was rendered by another component. To do this return null instead of its render output.
+## List
 
-In the example below, the `<WarningBanner />` is rendered depending on the value of the prop called warn. If the value of the prop is false, then the component does not render:
+### In Pure JS
 
-Returning `null` from a component’s render method does not affect the firing of the component’s lifecycle methods. For instance
-`componentDidUpdate` will still be called.
+```javascript
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((number) => number * 2);
+console.log(doubled);
+```
+
+### In React
+
+```javascript
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) => <li>{number}</li>);
+```
+
+The execution of the function `map()` is same in both the codes
+
+## Keys
+
+Keys help React identify which items have changed, are added, or are removed. Keys should be given to the elements inside the array to give the elements a stable identity:
+
+```javascript
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) => (
+  <li key={number.toString()}>{number}</li>
+));
+```
+
+The best way to pick a key is to use a string that uniquely identifies a list item among its siblings. Most often you would use IDs from your data as keys:
+
+```javascript
+const todoItems = todos.map((todo) => <li key={todo.id}>{todo.text}</li>);
+```
+
+When you don’t have stable IDs for rendered items, you may use the item index as a key as a last resort:
+
+```javascript
+const todoItems = todos.map((todo, index) => (
+  // Only do this if items have no stable IDs
+  <li key={index}>{todo.text}</li>
+));
+```
+
+### Incorrect ways to add KEYS
+
+```javascript
+function ListItem(props) {
+  const value = props.value;
+  return (
+    // Wrong! There is no need to specify the key here:
+    <li key={value.toString()}>{value}</li>
+  );
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => (
+    // Wrong! The key should have been specified here:
+    <ListItem value={number} />
+  ));
+  return <ul>{listItems}</ul>;
+}
+```
+
+## Keys Must Only Be Unique Among Siblings
+
+Keys used within arrays should be unique among their siblings. However they don’t need to be globally unique. We can use the same keys when we produce two different arrays
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,43 +1,75 @@
 import React from "react";
 import "./App.css";
 
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
+// Map function in React
+// const numbers = [1, 2, 3, 4, 5];
+// const listItems = numbers.map((number) => <li>{number * 2}</li>);
 
-  return <div className="warning">Warning!</div>;
+// List Components
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const listItems = numbers.map((number) => (
+//     <li key={number.toString()}>{number}</li>
+//   ));
+//   return <ul>{listItems}</ul>;
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// ========================================
+// Corect ways to use Keys
+// function ListItem(props) {
+//   // Correct! There is no need to specify the key here:
+//   return <li>{props.value}</li>;
+// }
+
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const listItems = numbers.map((number) => (
+//     // Correct! Key should be specified inside the array.
+//     <ListItem key={number.toString()} value={number} />
+//   ));
+//   return <ul>{listItems}</ul>;
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+// ==============================================================
+// Key Must be unique among its sibbilings
+
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+  const content = props.posts.map((post) => (
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  ));
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
 }
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { showWarning: true };
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick() {
-    this.setState((state) => ({
-      showWarning: !state.showWarning,
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? "Hide" : "Show"}
-        </button>
-      </div>
-    );
-  }
-}
+const posts = [
+  { id: 1, title: "Hello World", content: "Welcome to learning React!" },
+  { id: 2, title: "Installation", content: "You can install React from npm." },
+];
 
 const App = () => {
   return (
     <>
-      <Page />
+      {/* <ul>{listItems}</ul> */}
+      {/* <NumberList numbers={numbers} /> */}
+      <Blog posts={posts} />
     </>
   );
 };
