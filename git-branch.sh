85f96bef1e18bf9accb7cf248e03d11ce3c7434e
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-05-14 22:14:37 +0530

    My First Commit

---
+++ git-branch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+addFileDefault="."
+addFileDefaultTemp=""
+commitMessage="My First Commit"
+commitMessageTemp=""
+branchName="main"
+
+echo -e "Please enter file name to make git: ($addFileDefault)"
+read addFileDefaultTemp
+echo -e "Please Enter Commit message: ($commitMessage)"
+read commitMessageTemp
+echo -e "Please enter Branch name: ($branchName)"
+read branchName
+
+if [ "$addFileDefaultTemp" != "" ]; then
+    echo -e "File added is: $addFileDefaultTemp"
+    addFileDefault=$addFileDefaultTemp
+fi
+
+if [ "$commitMessageTemp" != "" ]; then
+    echo -e "Your Commit message is: $addFileDefaultTemp"
+    commitMessage=$commitMessageTemp
+fi
+
+git add $addFileDefault
+git commit -m "$commitMessage"
+echo -e $branchName
