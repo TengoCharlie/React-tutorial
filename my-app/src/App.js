95bcb5b0d4cc73439c78903f9e7c078d7d2f64ee
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-05-14 16:26:45 +0530

    Adding some JSX in the code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
-  );
-}
+const App = () => {
+  const formatName = (user) => {
+    return user.firstName + " " + user.lastName;
+  };
+
+  const user = {
+    firstName: "Harsh",
+    lastName: "Tenguriya",
+  };
+
+  const element = <h1>Hello, {formatName(user)}</h1>;
+
+  return <>{element}</>;
+};
 
 export default App;
