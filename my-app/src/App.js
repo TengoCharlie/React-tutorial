9c22931d3a2e9cb41102e90d2dcaeaf43449e2eb
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-05-14 16:39:46 +0530

    Specifying Attributes with JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,26 @@ const App = () => {
     return user.firstName + " " + user.lastName;
   };
 
+  // Can use JSX as an expression too
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, stranger</h1>;
+  }
+
   const user = {
     firstName: "Harsh",
     lastName: "Tenguriya",
+    avatarUrl: "https://picsum.photos/200",
   };
 
-  const element = <h1>Hello, {formatName(user)}</h1>;
+  // const element = getGreeting(user);
+
+  // Specifying attributes in jsx
+  // const element = <div tabIndex="0"></div>;
+  const element = <img src={user.avatarUrl} alt="Anyimage" />;
 
   return <>{element}</>;
 };
